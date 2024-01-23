c79af209b9af497fadf019a699e94a8946f51c04
Bhargav Adhikari <bhargavadhikari@Bhargavs-MacBook-Pro.local>
2024-01-23 10:04:22 +0545

    movie crud routes were added

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { Movie } from "./movie.model.js";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
@@ -11,13 +12,87 @@ router.post("/netflixmovie/add", async (req, res) => {
   return res.status(201).send({ message: "New movie added successfully" });
 });
 
-// router.get();
+// movie details route
 
-router.put("/netflixmovie/edit/:id", (req, res) => {
-  //   let movieIdToBeEdited = req.params.id;
-  //   let movieDataToBeEdited = req.body;
+router.get("/netflixmovie/details/:id", async (req, res) => {
+  let movieId = req.params.id;
+
+  // check if mongo id is Valid or not
+
+  const isValidMongoId = mongoose.Types.ObjectId.isValid(movieId);
+
+  if (!isValidMongoId) {
+    return res.status(400).send({ message: "Mongo Id is not Valid" });
+  }
+
+  // check if mongo id exists in the database or not
+
+  const requiredMovie = await Movie.findOne({ _id: movieId });
+
+  if (!requiredMovie) {
+    return res
+      .status(400)
+      .send({ message: "The movie does not exist in the database" });
+  }
+
+  return res.status(200).send({ message: "The movie is: ", requiredMovie });
 });
 
-// router.delete();
+router.put("/netflixmovie/edit/:id", async (req, res) => {
+  let movieId = req.params.id;
+  let movieDataToBeEdited = req.body;
+
+  console.log(req.body);
+
+  // check if mongo id is Valid or not
+
+  const isValidMongoId = mongoose.Types.ObjectId.isValid(movieId);
+
+  if (!isValidMongoId) {
+    return res.status(400).send({ message: "Mongo Id is not Valid" });
+  }
+
+  // check if mongo id exists in the database or not
+
+  const requiredMovie = await Movie.findOne({ _id: movieId });
+
+  if (!requiredMovie) {
+    return res
+      .status(400)
+      .send({ message: "The movie does not exist in the database" });
+  }
+
+  await Movie.updateOne({ _id: movieId }, { $set: { ...movieDataToBeEdited } });
+
+  return res
+    .status(400)
+    .send({ message: "The MOVIE is edited in the database " });
+});
+
+// delete a movie route
+
+router.delete("/netflixmovie/delete/:id", async (req, res) => {
+  let requiredIdToBeDeleted = req.params.id;
+
+  let isValidMongoId = mongoose.Types.ObjectId.isValid(requiredIdToBeDeleted);
+
+  if (!isValidMongoId) {
+    return res.status(400).send({ message: "Invalid Mongo Id" });
+  }
+
+  let requiredMovie = await Movie.findOne({ _id: requiredIdToBeDeleted });
+
+  if (!requiredMovie) {
+    return res.status(400).send({
+      message: "The movie doesn't exit in the database for deletion operation",
+    });
+  }
+
+  await Movie.deleteOne({ _id: requiredIdToBeDeleted });
+
+  return res
+    .status(200)
+    .send({ message: "The movie is deleted from the database" });
+});
 
 export default router;
